02e0d61ac4078fcf26bb1a815655f29d8058b438
Vladislav Kyi <v.kyy@nltu.lviv.ua>
2024-03-09 13:11:32 +0200

    redfactored code in user service

--- ProjectCollaborationPlatforn.Security/Interfaces/ITokenGenerator.cs
+++ ProjectCollaborationPlatforn.Security/Interfaces/ITokenGenerator.cs
@@ -1,4 +1,5 @@
-﻿using ProjectCollaborationPlatforn.Security.Models;
+﻿using ProjectCollaborationPlatforn.Security.DTOs;
+using ProjectCollaborationPlatforn.Security.Models;
 using ProjectCollaborationPlatforn.Security.Services.Autentication;
 
 namespace ProjectCollaborationPlatforn.Security.Interfaces
@@ -6,5 +7,6 @@ namespace ProjectCollaborationPlatforn.Security.Interfaces
     public interface ITokenGenerator
     {
         Task<AuthenticationResponse> GenerateTokens(User user);
+        Task<AuthenticationResponse> RefreshAccessToken(string accessToken, string refreshToken);
     }
 }
--- ProjectCollaborationPlatforn.Security/Interfaces/IUserService.cs
+++ ProjectCollaborationPlatforn.Security/Interfaces/IUserService.cs
@@ -1,15 +1,21 @@
 ﻿using ProjectCollaborationPlatforn.Security.DTOs;
 using ProjectCollaborationPlatforn.Security.Models;
+using ProjectCollaborationPlatforn.Security.Services.Autentication;
 
 namespace ProjectCollaborationPlatforn.Security.Interfaces
 {
     public interface IUserService
     {
-        public Task<bool> AddUser(SignUpDTO user);
-        public Task<User> GetUserById(string id);
-        Task<bool> SendEmailVerification(User user);
-        Task<string> CallBackUrl(User user, string code);
-        Task<bool> VerifyEmail(User user, string code);
+        public Task<UserDTO> AddUser(SignUpDTO user);
+        public Task<bool> DeleteUser(string email);
+        public Task<UserDTO> GetUserById(string id);
+        public Task<UserDTO> GetUserByEmail(string email);
+        public Task<AuthenticationResponse> GenerateTokens(string email);
+        Task<bool> IsUserExists(string email);
+        Task<bool> VerifyEmail(UserDTO user, string code);
+        Task<bool> SendPasswordResetCode(UserDTO user);
+        Task<bool> VerifyPasswordResetCode(UserDTO user, string code, string newPassword); 
+        Task<bool> CheckUserPassword(string email, string password);
 
     }
 }
