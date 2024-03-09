cf326cc95c2d149a3e21af94393b8db0abcf96eb
Vladislav Kyi <v.kyy@nltu.lviv.ua>
2024-03-09 13:06:40 +0200

    added DTOs for refresh token and reset code, minor changes in other

---
+++ ProjectCollaborationPlatforn.Security/DTOs/ResetCodeDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectCollaborationPlatforn.Security.DTOs
+{
+    public class ResetCodeDTO
+    {
+        public string ResetCode {  get; set; }
+        public string Email {  get; set; }
+        public string NewPassword {  get; set; }
+    }
+}
--- ProjectCollaborationPlatforn.Security/DTOs/UserDTO.cs
+++ ProjectCollaborationPlatforn.Security/DTOs/UserDTO.cs
@@ -2,6 +2,9 @@
 {
     public class UserDTO
     {
-
+        public Guid Id { get; set; }    
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string RoleName { get; set; }
     }
 }
